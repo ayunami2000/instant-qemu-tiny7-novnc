2bcadeea9290bea60fe4d54cf6284c548d07bbba
ayunami2000 <spwilliamsiam@gmail.com>
2020-12-09 12:06:57 -0500

    Update vt7-ubuntu.sh

--- vt7-ubuntu.sh
+++ vt7-ubuntu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 apt update
 apt install qemu
-snap install novnc
 qemu-img create -f raw win7.img 10G
 git clone https://github.com/Juvenal-Yescas/mediafire-dl
 cd mediafire-dl
 pip3 install -r requirements.txt
 python3 mediafire-dl.py http://www.mediafire.com/file/sj7694734mb06jv/downloadisofile.blogspot.com_Tiny7.iso/file
 cd ..
-novnc --listen 80 &
+git clone https://github.com/novnc/noVNC
+./noVNC/utils/launch.sh --listen 80 &
 qemu-system-x86_64 -hda ./win7.img -m 3072 \
 -net nic,model=virtio -net user -cdrom ./mediafire-dl/downloadisofile.blogspot.com_Tiny7.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
