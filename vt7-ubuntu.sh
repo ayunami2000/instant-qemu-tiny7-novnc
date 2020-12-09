95315afb3265a4fd588d5c96dbb5054376f4df26
ayunami2000 <spwilliamsiam@gmail.com>
2020-12-09 12:55:42 -0500

    Update vt7-ubuntu.sh

--- vt7-ubuntu.sh
+++ vt7-ubuntu.sh
@@ -9,7 +9,7 @@ python3 mediafire-dl.py http://www.mediafire.com/file/sj7694734mb06jv/downloadis
 cd ..
 snap install novnc
 novnc --listen 80 &
-qemu-system-x86_64 -hda ./win7.img -m 3072 \
+qemu-system-x86_64 -vnc :0 -hda ./win7.img -m 3072 \
 -net nic,model=virtio -net user -cdrom ./mediafire-dl/downloadisofile.blogspot.com_Tiny7.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet -vga vmware
