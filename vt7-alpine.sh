64a4744c2611bc3bde10eb40e03dea827e1438a2
ayunami2000 <spwilliamsiam@gmail.com>
2020-12-09 12:55:52 -0500

    Update vt7-alpine.sh

--- vt7-alpine.sh
+++ vt7-alpine.sh
@@ -10,7 +10,7 @@ python3 mediafire-dl.py http://www.mediafire.com/file/sj7694734mb06jv/downloadis
 cd ..
 git clone https://github.com/novnc/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-qemu-system-x86_64 -hda ./win7.img -m 3072 \
+qemu-system-x86_64 -vnc :0 -hda ./win7.img -m 3072 \
 -net nic,model=virtio -net user -cdrom ./mediafire-dl/downloadisofile.blogspot.com_Tiny7.iso \
 -rtc base=localtime,clock=host -smp cores=4,threads=4 \
 -usbdevice tablet -vga vmware
